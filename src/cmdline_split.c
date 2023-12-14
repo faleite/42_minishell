d0da19b4997640ab9620c3edc7e95f2ce5acaeae
faleite <fabricio_2310@hotmail.com>
2023-12-14 21:48:12 +0000

    Init function for cat variables env

--- src/cmdline_split.c
+++ src/cmdline_split.c
@@ -1,16 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_split.c                                     :+:      :+:    :+:   */
+/*   cmdline_split.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: faaraujo <faaraujo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 19:00:07 by faaraujo          #+#    #+#             */
-/*   Updated: 2023/12/12 19:03:52 by faaraujo         ###   ########.fr       */
+/*   Updated: 2023/12/14 19:50:04 by faaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/parser.h"
+
+static void		end_quote(char *sig, char **s1, char **s2);
+static void		replace_spaces(char **s2, char **s1);
+static int		is_redpipe(char c);
+
+/**
+ * Substitui separador
+ * echo "hello      there" how are 'you 'doing? $USER |wc -l >outfile
+ * echo/2"hello      there"/2how/2are/2'you 'doing?/2$USER/2|wc/2-l/2>outfile
+*/
+void	handle_quotes(char *s1, char *s2)
+{
+	char	sig;
+
+	sig = 1;
+	while (*s1)
+	{
+		if (sig == 1)
+		{
+			replace_spaces(&s2, &s1);
+			if (*s1 == '\"' || *s1 == '\'')
+				sig = *s1;
+			*s2++ = *s1++;
+		}
+		else
+			end_quote(&sig, &s1, &s2);
+	}
+	*s2 = '\0';
+}
 
 static void	end_quote(char *sig, char **s1, char **s2)
 {
@@ -38,24 +67,4 @@ static void	replace_spaces(char **s2, char **s1)
 		*(*s2)++ = '\2';
 		*(*s1)++;
 	}
-}
-
-void	handle_quotes(char *s1, char *s2)
-{
-	char	sig;
-
-	sig = 1;
-	while (*s1)
-	{
-		if (sig == 1)
-		{
-			replace_spaces(&s2, &s1);
-			if (*s1 == '\"' || *s1 == '\'')
-				sig = *s1;
-			*s2++ = *s1++;
-		}
-		else
-			end_quote(&sig, &s1, &s2);
-	}
-	*s2 = '\0';
 }
--- src/minishell.c
+++ src/minishell.c
@@ -6,14 +6,61 @@
 /*   By: faaraujo <faaraujo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 18:21:53 by faaraujo          #+#    #+#             */
-/*   Updated: 2023/12/13 21:44:35 by faaraujo         ###   ########.fr       */
+/*   Updated: 2023/12/14 21:45:56 by faaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/parser.h"
 
+#define VAR_LEN 1000
+
+// char	*var_copy(char **s2)
+// {
+// 	while (**s2 != ' ' && **s2)
+// 	{
+		
+// 	}
+// }
+
+char	*expand_var(char *s2)
+{
+	char	*s3;
+	char	var[VAR_LEN];
+	char	*var_env;
+	int		i;
+	int		j;
+
+	s3 = (char *)malloc(sizeof(char) * (ft_strlen(s2) * 2));
+	if (!s3)
+		return (NULL);
+	i = 0;
+	while (*s2)
+	{
+		if (*s2 == '$') //&& *(s2 - 1) != '\'')
+		{
+			s2++;
+			j = 0;
+			while (*s2 != ' ' && *s2)
+				var[j++] = *s2++;
+			var[j] = '\0';
+			printf("var: %s\n", var);
+			var_env = getenv(var);
+			printf("var_env: %s\n", var_env);
+			if (var_env != NULL)
+			{
+				ft_strlcpy(s2, var_env, ft_strlen(var_env));
+				i += ft_strlen(var_env);
+			}
+		}
+		else
+			s3[i++] = *s2++;
+	}
+	s3[i] = '\0';
+	return (s3);
+}
+
 /**
- * Criar uma funcao que substitui meu separador
+ * Substitui separador
  * echo "hello      there" how are 'you 'doing? $USER |wc -l >outfile
  * echo/2"hello      there"/2how/2are/2'you 'doing?/2$USER/2|wc/2-l/2>outfile
 */
@@ -52,25 +99,27 @@ void	cmdline(void)
 }
 
 // int	main(int argc, char *argv[])
-int	main(void)
-{
 	//char	s1[] = "echo \"hello  '  there\" how are 'you 'doing? $USER |wc -l >outfile";
 	//char	s1[] = "<<Makefile cat| echo \"$PWD < > | 'hola'\" ~/src | 'tr' -d / >outfile";
-	char	s1[] = "\"$PWD < > \\ | 'hola'\" ~/src ${USER} $HOME |'tr' -d / >outfile";
+	//char	s1[] = "\"$PWD < > \\ | 'hola'\" ~/src ${USER} $HOME |'tr' -d / >outfile";
 	//char	s1[] = "echo|cat || teste >>>> >>ts >>>tdb ||t";
-	//char	*s2;
-	char	**arr;
+int	main(void)
+{
+	char	s1[] = "echo \"$HOME '$PWD' >>>\" $USER '$PWD' ~///fal";
+				// output: /home/fal '/home/fal' >>> fal $PWD /home/fal///fal
+	// char	**arr;*
+	char	*s2;
 
-	arr = sep_replace(s1);
+	// arr = sep_replace(s1);*
 	//s2 = sep_replace(s1);
-	//printf("%s\n%s\n", s1, s2);
-	//free(s2);
-	printf("String Original: %s\n", s1);
-	printf("\nFirst parser:\n");
-	print_arr(arr);
-	free_arr(arr);
+	s2 = expand_var(s1);
+	printf("%s\n", s2);
+	free(s2);
+	// printf("String Original: %s\n", s1);*
+	// printf("\nFirst parser:\n");*
+	// print_arr(arr);*
+	// free_arr(arr);*
 	//cmdline();
-
 	// printf("env: %s\n", getenv("USER"));
 	// printf("env: %s\n", getenv("HOME"));
 	return (0);
