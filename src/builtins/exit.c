d9d5fc7c2b800561ad64e358174a26e09069063f
Filipe Silva <filipeedensilva@gmail.com>
2024-02-21 19:01:16 +0000

    Almost 100%

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: faaraujo <faaraujo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 02:20:17 by feden-pe          #+#    #+#             */
-/*   Updated: 2024/02/19 20:24:23 by faaraujo         ###   ########.fr       */
+/*   Updated: 2024/02/21 17:07:34 by feden-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,17 @@ int	exit_builtin(t_command *command)
 	data()->exit_status = 0;
 	if (command->args[1])
 	{
-		if (command->args[2])
-		{
-			ft_putendl_fd("minishell: exit: too many arguments", 2);
-			return (1);
-		}
 		if (!is_long(command->args[1]))
 		{
 			data()->exit_status = 2;
 			print_error(command->args[1]);
+		}
+		else if (command->args[2])
+		{
+			ft_putendl_fd("exit\nminishell: exit: too many arguments", 2);
+			if (data()->exit_status == 0)
+				data()->exit_status = 1;
+			return (1);
 		}
 		else if (!command->args[2])
 			data()->exit_status = ft_atoi(command->args[1]);
